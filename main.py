2220872d0e9604ba83096f9f365e79925d3da51f
print-agent <print_agent_main@proton.me>
2025-07-06 13:06:55 +0500

    Added function to print character count

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_chars, count_words
+from stats import count_chars, count_words, sort_chars
 
 RELPATH = "./books/frankenstein.txt"
 
@@ -11,7 +11,17 @@ def main():
     book_text = get_book_text(RELPATH)
     number_of_words = count_words(book_text)
     number_of_chars = count_chars(book_text)
-    print(f"{number_of_words} words found in the document")
-    print(f"{number_of_chars} characters found in the document")
+    sorted_dictionaries = sort_chars(number_of_chars)
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {number_of_words} total words")
+    print("--------- Character Count -------")
+
+    for sort_item in sorted_dictionaries:
+        print(f"{sort_item['char']}: {sort_item['num']}")
+
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,7 @@
+def order_by(dict_item):
+    # TODO: dict{"letter": "a", "count": 25}
+    return dict_item["num"]
+
 def count_words(text):
     words = text.split()
     n_words = len(words)
@@ -11,3 +15,17 @@ def count_chars(text):
             cnt_char[char] = 0
         cnt_char[char] += 1
     return cnt_char
+
+def sort_chars(char_dict):
+    char_list = []
+    for key in char_dict:
+        char_num_dict = {}
+        if key.isalpha():
+            char_num_dict["char"] = key
+            char_num_dict["num"] = char_dict[key]
+        else:
+            continue
+        char_list.append(char_num_dict)
+    char_list.sort(reverse=True, key=order_by)
+    return char_list
+
